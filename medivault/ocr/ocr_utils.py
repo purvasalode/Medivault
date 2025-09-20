3ff5171e4d9fe98f3a80e1d7fd9e8cb794ffc388
purvasalode <purvasalode07@gmail.com>
2025-09-20 23:46:40 +0530

    Replace Mistral OCR with OpenAI OCR in ocr_utils.py

--- medivault/ocr/ocr_utils.py
+++ medivault/ocr/ocr_utils.py
@@ -1,4 +1,4 @@
-import os
+                                                                                                                       import os
 import base64
 import mimetypes
 from mistralai import Mistral
